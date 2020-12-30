4d7316a95666c4ed010290362516b7999ecf0ed5
Тамазян Арсен Гарникович <a.tamazyan@focus.bi>
2020-12-30 08:59:47 +0300

    добавлен touchMove event

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'ng-slider';
+
+  // @ts-ignore
+  @ViewChild('sliderBar') private sliderBar: ElementRef;
+  // @ts-ignore
+  @ViewChild('sliderLine') private sliderLine: ElementRef;
+
+  sliderBarMove(event: DragEvent): void {
+    setTimeout(() => {
+      this.setOffsetLeft(event.clientX);
+    }, 50);
+  }
+
+  private setOffsetLeft(value: number): void {
+    const sliderBar: any = this.sliderBar.nativeElement;
+    sliderBar.style.left = `${value}px`;
+  }
+
+  drag(event: DragEvent): void {
+    this.setOffsetLeft(event.clientX);
+  }
+
+  touchMove(event: TouchEvent): void {
+    this.setOffsetLeft(event.touches[0].clientX);
+  }
 }
