037e6d9be3cdaab204f5e06005c6c8d40f18183f
Тамазян Арсен Гарникович <a.tamazyan@focus.bi>
2021-01-17 12:22:18 +0300

    Добавлено ограничение на смещение ползунка
    Ползунок не выходит за рамки линии перемещения

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,23 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
+  private sliderBarElement: any;
+  private sliderLineElement: any;
 
   // @ts-ignore
   @ViewChild('sliderBar') private sliderBar: ElementRef;
   // @ts-ignore
   @ViewChild('sliderLine') private sliderLine: ElementRef;
+
+  ngAfterViewInit(): void {
+    this.sliderBarElement = this.sliderBar.nativeElement;
+    this.sliderLineElement = this.sliderLine.nativeElement;
+  }
 
   sliderBarMove(event: DragEvent): void {
     setTimeout(() => {
@@ -19,8 +26,7 @@ export class AppComponent {
   }
 
   private setOffsetLeft(value: number): void {
-    const sliderBar: any = this.sliderBar.nativeElement;
-    sliderBar.style.left = `${value}px`;
+    this.sliderBarElement.style.left = `${Math.min(value, this.sliderLineElement.clientWidth)}px`;
   }
 
   drag(event: DragEvent): void {
